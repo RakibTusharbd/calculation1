edb10cf4b601f1ace600714e672d91ccd4e62084
RakibTusharbd <121655822+RakibTusharbd@users.noreply.github.com>
2023-02-18 21:19:54 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+
+function cal (){
+    const input1 = parseInt(document.getElementById('input1').value);
+    const input2 = parseInt(document.getElementById('input2').value);
+    // console.log(input1*input2 / 2)
+    document.getElementById('result').value = input1*input2 / 2;
+  
+}
+
+function cal2 (){
+    const input3 = parseInt(document.getElementById('input3').value);
+    const input4 = parseInt(document.getElementById('input4').value);
+    // console.log(input3 *input4);
+    document.getElementById('result').value = input3*input4;
+
+}
+
+
+function cal3 (){
+    const input5 = parseInt(document.getElementById('input5').value);
+    const input6 = parseInt(document.getElementById('input6').value);
+    // console.log(input3 *input4);
+    document.getElementById('result').value = input5*input6;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
